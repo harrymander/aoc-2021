e567c70411ae31c71b3be3d6db5044d48272fc50
Harry Mander <harrymander96@gmail.com>
2021-12-06 16:04:58 +1300

    day05: part 1

--- day05/day05.rs
+++ day05/day05.rs
@@ -1,37 +1,25 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::cmp::{min,max};
 
-#[derive(Debug)]
-struct Coord {
-    x: u32,
-    y: u32
+fn coord_from_string(coord: &str) -> (usize, usize) {
+    let mut split = coord.split(",")
+        .map(|s| s.parse().unwrap());
+    (split.next().unwrap(), split.next().unwrap())
 }
 
-#[derive(Debug)]
+fn count_overlaps(grid: &Vec<Vec<usize>>) -> usize {
+    grid.iter()
+        .map(|row| row.iter().filter(|&c| *c >= 2).count())
+        .sum()
+}
+
 struct Line {
-    start: Coord,
-    end: Coord,
-}
-
-impl Coord {
-    pub fn from_string(string: &str) -> Self {
-        let mut split = string.split(",")
-            .map(|s| s.trim().parse().unwrap());
-        Self { x: split.next().unwrap(), y: split.next().unwrap() }
-    }
-}
-
-impl Line {
-    pub fn from_string(string: &str) -> Self {
-        let mut split = string.split("->")
-            .map(|s| Coord::from_string(s));
-        Self { start: split.next().unwrap(), end: split.next().unwrap() }
-    }
-
-    pub fn overlaps(&self, line: &Line) -> u32 {
-        0
-    }
+    x1: usize,
+    y1: usize,
+    x2: usize,
+    y2: usize,
 }
 
 fn main() {
@@ -43,15 +31,40 @@ fn main() {
     let lines_file = fs::read_to_string(&args[1])
         .expect("Cannot open file");
 
+    let mut max_x = 0usize;
+    let mut max_y = 0usize;
     let lines: Vec<Line> = lines_file.lines()
-        .map(|line| Line::from_string(line)).collect();
+        .map(|line| {
+            let mut coords = line.split(" -> ")
+                .map(|coord| coord_from_string(coord));
 
-    let mut overlaps: u32 = 0;
-    for i in 0..(lines.len() - 1) {
-        for j in (i + 1)..lines.len() {
-            overlaps += lines[i].overlaps(&lines[j]);
+            let start = coords.next().unwrap();
+            let end = coords.next().unwrap();
+
+            let xmax = max(start.0, end.0);
+            let xmin = min(start.0, end.0);
+            let ymin = min(start.1, end.1);
+            let ymax = max(start.1, end.1);
+
+            max_x = max(max_x, xmax);
+            max_y = max(max_y, ymax);
+
+            Line { x1: xmin, y1: ymin, x2: xmax, y2: ymax }
+        }).collect();
+
+    let hor_ver_lines: Vec<&Line> = lines.iter()
+        .filter(|&l| (l.x1 == l.x2) || (l.y1 == l.y2))
+        .collect();
+
+    let mut grid_hor_ver = vec![vec![0usize; max_x + 1]; max_y + 1];
+    for line in hor_ver_lines {
+        for row in line.y1..=line.y2 {
+            for col in line.x1..=line.x2 {
+                grid_hor_ver[row][col] += 1;
+            }
         }
     }
 
-    println!("{}", overlaps);
+    println!("Horizontal and vertical lines only: {}",
+             count_overlaps(&grid_hor_ver));
 }
