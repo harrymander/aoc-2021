ef1ea1f5dc7dcd602f5b3ad579458850b6e462f1
Harry Mander <harrymander96@gmail.com>
2021-12-08 23:27:20 +1300

    day08: part 2 (brute force)

--- day08/day08.py
+++ day08/day08.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import itertools
 import sys
 
 
@@ -16,17 +17,46 @@ DIGIT_SEGS = (
     'abcdfg'
 )
 
-DIGITS = {tuple(sorted(seg)): i for i, seg in enumerate(DIGIT_SEGS)}
+SEGMENTS = 'abcdefg'
+NUM_SEGMENTS = len(SEGMENTS)
+
+
+def decode_digit(segs, order='abcdefg'):
+    reordered_segs = sorted(SEGMENTS[order.index(s)] for s in segs)
+    try:
+        return DIGIT_SEGS.index(''.join(reordered_segs))
+    except ValueError:
+        return -1
+
+
+def check_order(segments, order):
+    digits = [-1]
+    for segment in segments:
+        digit = decode_digit(segment, order)
+        if digit in digits:
+            return False
+    return True
 
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
-        unique_patterns, displayed_segments = zip(*(
-            tuple(map(str.split, line.split('|'))) for line in f.readlines()
+        all_segments, all_sensor_segments = zip(*(
+            map(str.split, line.split('|'))
+            for line in f.readlines()
         ))
 
     unique_lens = tuple(len(s) for s in (DIGIT_SEGS[i] for i in (1, 4, 7, 8)))
-    num_unique = 0
-    for seg in displayed_segments:
-        num_unique += sum(len(s) in unique_lens for s in seg)
-    print(num_unique)
+    num_unique_len_digits = 0
+    for seg in all_sensor_segments:
+        num_unique_len_digits += sum(len(s) in unique_lens for s in seg)
+    print('Number of digits with unique number of segments:',
+          num_unique_len_digits)
+
+    total = 0
+    for segments, sensor_segments in zip(all_segments, all_sensor_segments):
+        for order in itertools.permutations(SEGMENTS, NUM_SEGMENTS):
+            if check_order(segments, order):
+                total += sum(10**i * decode_digit(seg, order) for
+                             i, seg in enumerate(reversed(sensor_segments)))
+
+    print('Sum of all sensor outputs', total)
