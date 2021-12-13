5c741eca6c0e13ff5667d8e476cae855f7b484cf
Harry Mander <harrymander96@gmail.com>
2021-12-14 09:04:29 +1300

    day13: refactor without numpy

--- day13/day13.py
+++ day13/day13.py
@@ -2,51 +2,53 @@
 
 import sys
 
-import numpy as np
 
-
-def fold_paper(paper, fold):
+def fold_paper(dots, fold):
     axis, number = fold
+    new_dots = set()
+    for dot in dots:
+        if dot[axis] < number:
+            new_dots.add(dot)
+        else:
+            new_pos = 2 * number - dot[axis]
+            new_dot = list(dot)
+            new_dot[axis] = new_pos
+            new_dots.add(tuple(new_dot))
+    return new_dots
 
-    if axis:  # axis == 'x'
-        fold1, fold2 = paper[:, :number], paper[:, number + 1:]
-    else:
-        fold1, fold2 = paper[:number, :], paper[number + 1:, :]
 
-    fold2 = np.flip(fold2, axis)
-    return fold1 | fold2
+def print_dots(dots, dotchar='█', nodotchar=' '):
+    max_row = max(dot[0] for dot in dots) + 1
+    max_col = max(dot[1] for dot in dots) + 1
+    for row in range(max_row):
+        print(''.join(dotchar if (row, col) in dots else nodotchar
+                      for col in range(max_col)))
 
 
-if __name__ == '__main__':
-    max_x, max_y = 0, 0
-    dots = []
+def main():
+    dots = set()
     folds = []
     with open(sys.argv[1]) as f:
         while True:
             line = f.readline().strip()
             if not line:
                 break
-            x, y = map(int, line.split(','))
-            if x > max_x:
-                max_x = x
-            if y > max_y:
-                max_y = y
-            dots.append((x, y))
+            col, row = map(int, line.split(','))
+            dots.add((row, col))
 
         for line in f.readlines():
             fold = line.split()[-1].strip()
             axis, number = fold.split('=')
             folds.append((int(axis == 'x'), int(number)))
 
-    paper = np.zeros((max_y + 1, max_x + 1), dtype=bool)
-    for x, y in dots:
-        paper[y, x] = True
-
-    paper = fold_paper(paper, folds[0])
-    print(f'After 1 fold there are {paper.sum()} dots\n')
+    dots = fold_paper(dots, folds[0])
+    print(f'After 1 fold there are {len(dots)} dots\n')
 
     for fold in folds[1:]:
-        paper = fold_paper(paper, fold)
+        dots = fold_paper(dots, fold)
 
-    for row in paper:
-        print(''.join('█' if dot else ' ' for dot in row))
+    print_dots(dots)
+
+
+if __name__ == '__main__':
+    main()
