94dbbd5394e69fa5481f77d92db7e8fe4f58f186
Harry Mander <harrymander96@gmail.com>
2021-12-14 22:45:42 +1300

    day14: part 2

--- day14/day14.py
+++ day14/day14.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
+import operator
 import sys
 
 
-def grow_polymer(template, rules):
-    new_template = []
-    for i, (a, b) in enumerate(zip(template[:-1], template[1:])):
-        insert = rules.get(a + b, None)
-        if insert:
-            new_template.extend(a + insert)
-        else:
-            new_template.append(a)
-    new_template.append(template[-1])
+def grow_polymer(pair_counts, char_counts, rules):
+    new_counts = {}
+    for pair, count in filter(operator.itemgetter(1), pair_counts.items()):
+        pair_counts[pair] = 0
+        insert = rules[pair]
+        for new_pair in (pair[0] + insert, insert + pair[1]):
+            new_counts[new_pair] = new_counts.get(new_pair, 0) + count
+        char_counts[insert] = char_counts.get(insert, 0) + count
 
-    return new_template
+    pair_counts.update(new_counts)
 
 
-def max_min_diff(template):
-    counts = tuple(template.count(elem) for elem in set(template))
-    return max(counts) - min(counts)
+def max_min_diff(char_counts):
+    return max(char_counts.values()) - min(char_counts.values())
 
 
 def main():
@@ -26,11 +25,23 @@ def main():
         template = f.readline().strip()
         f.readline()
 
-        rules = dict(line.strip().split(' -> ') for line in f.readlines())
+        rules = {}
+        for line in f.readlines():
+            (a, b), insert = line.strip().split(' -> ')
+            rules[a + b] = insert
+
+    char_counts = {elem: template.count(elem) for elem in set(template)}
+    pair_counts = {pair: 0 for pair in rules.keys()}
+    for a, b in zip(template[:-1], template[1:]):
+        pair_counts[a + b] += 1
 
     for _ in range(10):
-        template = grow_polymer(template, rules)
-    print('After 10 steps:', max_min_diff(template))
+        grow_polymer(pair_counts, char_counts, rules)
+    print('After 10 steps:', max_min_diff(char_counts))
+
+    for _ in range(30):
+        grow_polymer(pair_counts, char_counts, rules)
+    print('After 40 steps:', max_min_diff(char_counts))
 
 
 if __name__ == '__main__':
